78ed0107bd56fe2767a10cfcfa2b971b5cfc3740
Architbh007 <bhullararchit@gmail.com>
2026-09-03 20:33:16 +1000

    Exclude generated dashboard files from linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import eslint from "@eslint/js";
 
 export default [
   {
-    ignores: ["**/coverage/**", "**/node_modules/**"],
+    ignores: ["**/.next/**", "**/coverage/**", "**/node_modules/**"],
   },
   eslint.configs.recommended,
   {
